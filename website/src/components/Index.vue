b6988f94057610f9ad7b52a2658d7ec43e6c3021
egrocha <egrocha97@gmail.com>
2020-05-22 12:18:15 +0100

    Website index updated

---
+++ website/src/components/Index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+		<v-toolbar color="red">
+    		<v-toolbar-title>Administração do Sistema</v-toolbar-title>
+    		<v-spacer></v-spacer>
+    		<v-toolbar-items class="hidden-sm-and-down">
+    		  <v-btn flat disabled>Lista de Espaços</v-btn>
+    		  <v-btn flat href="/admin">Admin</v-btn>
+    		</v-toolbar-items>
+		</v-toolbar>
+        <v-container fluid grid-list-md text-xs-center>
+            <v-layout row wrap>
+            </v-layout>
+        </v-container>
+    </div>
+</template>
+
+<script>
+  export default{
+      
+  }
+</script>
+
+<style>
+
+</style>
